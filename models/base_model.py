1a0c3559c92c286725c42440771ae1f2e65ad18c
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-15 14:31:06 +0000

    Enhance attribute initialization in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -40,15 +40,15 @@ class BaseModel:
                 'updated_at'.
         """
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, time_format)
                 else:
                     self.__dict__[key] = value
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
 
     def save(self):
         """
