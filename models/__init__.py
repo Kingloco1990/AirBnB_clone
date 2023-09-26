131db194293997d2b40cca886de5a4603272efee
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-26 08:09:36 +0000

    Initialize FileStorage and Perform Initial Data Reload

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""
+__init__ magic method for the models directory.
+
+This module initializes the FileStorage instance for the models directory and
+performs an initial reload of stored data.
+"""
 from models.engine.file_storage import FileStorage
 
 
