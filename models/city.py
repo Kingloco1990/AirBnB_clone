e25ce2f1c517bd81e09a1c3d6d46f5238bb29bbd
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-29 09:43:15 +0000

    Add City class inheriting from BaseMode

--- models/city.py
+++ models/city.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+"""This module defines the City class, which inherits from BaseModel.
+"""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
+    """
+    City class for representing cities.
+
+    Args:
+         BaseModel (class): The base class for all models.
 
     Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
+              state_id (str): The ID of the state associated with the city.
+              name (str): The name of the city.
     """
-
     state_id = ""
     name = ""
