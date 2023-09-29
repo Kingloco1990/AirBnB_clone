22801ac15b9596bde6d21d4e7eb02ccb28be5e81
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-29 09:00:19 +0000

    Update FileStorage to manage correctly serialization and deserialization of User

--- models/user.py
+++ models/user.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""This module defines the User class, a subclass of BaseModel.
+"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
+    """User class represents users in the application.
+
+    Args:
+        BaseModel (class): The base class for all models.
 
     Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
+              email (str): The user's email address.
+              password (str): The user's password.
+              first_name (str): The user's first name.
+              last_name (str): The user's last name.
     """
-
     email = ""
     password = ""
     first_name = ""
