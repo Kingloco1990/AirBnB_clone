2e6daa6f58fb64f6cc09f777bc6a79762b8d865f
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-29 09:55:44 +0000

    Add Review class inheriting from BaseMode

--- models/review.py
+++ models/review.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Defines the Review class."""
+"""Defines the Review class, inheriting from BaseModel."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    """Represent a review.
+    """
+    Place class representing a review for a place.
+
+    Args:
+        BaseModel (class): The base class for all models.
 
     Attributes:
-        place_id (str): The Place id.
-        user_id (str): The User id.
-        text (str): The text of the review.
+        place_id (str): The ID of the place being reviewed.
+        user_id (str): The ID of the user who wrote the review.
+        text (str): The text content of the review.
     """
-
     place_id = ""
     user_id = ""
     text = ""
