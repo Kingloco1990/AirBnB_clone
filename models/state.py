ea8b01b9df8f7c54f67f3d1213fa10bc4c6e195a
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-09-29 09:29:36 +0000

    Add State class inheriting from BaseMode

--- models/state.py
+++ models/state.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
-"""Defines the State class."""
+"""This module defines the State class, which inherits from BaseModel
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
+    """
+    Represent a state.
+
+    Args:
+        BaseModel (class): The base class for all models.
 
     Attributes:
         name (str): The name of the state.
     """
-
     name = ""
