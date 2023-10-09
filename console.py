a88a78aac8fc52fcb81ec0fe27d25361b42ee461
Kingloco1990 <ransfordnawelbeck@gmail.com>
2023-10-09 20:55:24 +0000

    Update your console.py to retrieve all instances of a class

--- console.py
+++ console.py
@@ -62,6 +62,7 @@ class HBNBCommand(cmd.Cmd):
             "all": self.do_all,
             "show": self.do_show,
             "destroy": self.do_destroy,
+            "count": self.do_count,
             "update": self.do_update
         }
         match = re.search(r"\.", line)
